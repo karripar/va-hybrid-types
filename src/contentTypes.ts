494ea383ccff924ea4ebecfd92dda137f7bd6126
karripartanen <karripartanen25@gmail.com>
2025-10-01 18:22:48 +0300

    added a type for a partner school with coordinates included (lat, lon)

--- src/contentTypes.ts
+++ src/contentTypes.ts
@@ -4,13 +4,25 @@ type PartnerSchool = {
     link: string;
 }
 
+type PartnerSchoolWithCoordinates = PartnerSchool & {
+    latitude: number;
+    longitude: number;
+}
+
 type DestinationResponse = {
     destinations: {
         [program: string]: PartnerSchool[];
     }
 }
 
+type DestinationWithCoordinatesResponse = {
+    destinations: {
+        [program: string]: PartnerSchoolWithCoordinates[];
+    }
+}
+
 export type {
     PartnerSchool,
-    DestinationResponse
+    DestinationResponse,
+    PartnerSchoolWithCoordinates,
 };

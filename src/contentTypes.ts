187bdbec2c84d0e7d8014934afd713d39679c3b0
ellinor <ellinoora.riskala@metropolia.fi>
2025-10-06 14:20:52 +0300

    added missing properties for profileresponse type

--- src/contentTypes.ts
+++ src/contentTypes.ts
@@ -1,40 +1,43 @@
 type PartnerSchool = {
-    country: string;
-    title: string;
-    link: string;
-}
+  country: string;
+  title: string;
+  link: string;
+};
 
 type PartnerSchoolWithCoordinates = PartnerSchool & {
-    coordinates: {
-        lat: number;
-        lng: number;
-    }
-}
+  coordinates: {
+    lat: number;
+    lng: number;
+  };
+};
 
 type DestinationResponse = {
-    destinations: {
-        [program: string]: PartnerSchool[];
-    }
-}
+  destinations: {
+    [program: string]: PartnerSchool[];
+  };
+};
 
 type DestinationWithCoordinatesResponse = {
-    destinations: {
-        [program: string]: PartnerSchoolWithCoordinates[];
-    }
-}
+  destinations: {
+    [program: string]: PartnerSchoolWithCoordinates[];
+  };
+};
 type ProfileResponse = {
-    id: string;
-    userName: string;
-    email: string;
-    registeredAt: string;
-    favorites: string[];
-    documents: string[];
+  id: string;
+  userName: string;
+  email: string;
+  registeredAt: string;
+  favorites: string[];
+  documents: string[];
+  avatarUrl: string | null;
+  exchangeBadge: boolean;
+  linkedinUrl: string | null;
 };
 
 export type {
-    PartnerSchool,
-    DestinationResponse,
-    PartnerSchoolWithCoordinates,
-    DestinationWithCoordinatesResponse,
-    ProfileResponse
+  PartnerSchool,
+  DestinationResponse,
+  PartnerSchoolWithCoordinates,
+  DestinationWithCoordinatesResponse,
+  ProfileResponse,
 };
